446b537577d98bb01c2e2ff714b8496d41250bce
Wolf0605 <82213429+Wolf0605@users.noreply.github.com>
2021-10-20 21:04:42 +0900

    /

---
+++ Adding text/test.py
@@ -0,0 +1,72 @@
+from PIL import Image ,ImageFont, ImageDraw, ImageOps
+
+
+# What's at?
+def draw_text_90_into (text: str, into, at: list):
+    # Measure the text area
+    font = ImageFont.truetype ('ttf/Merriweather-BoldItalic.ttf', 100)
+    # wi = x, hi = y
+    wi, hi = font.getsize(text)
+    print(wi, hi)
+
+    # Copy the relevant area from the source image
+    # crop(left, top, right, bottom)
+    img = into.crop ((at[0], at[1], at[0] + hi, at[1] + wi))
+
+    # Rotate it backwards ( 시계 반대 방향 )
+    img = img.rotate (270, expand = 1)
+
+    # Print into the rotated area
+    d = ImageDraw.Draw(img)
+
+    # fill = color*
+    d.text ((0, 0), text, font = font, fill = (0, 0, 0))
+    # # # #
+    # # # # # Rotate it forward again (시계 반대 방향 )
+    img = img.rotate (90, expand = 1)
+
+    # Insert it back into the source image
+    # Note that we don't need a mask
+    into.paste (img, at)
+    # img.show()
+
+into= Image.open("Resources/unnamed.jpg")
+
+draw_text_90_into('hihihi', into, [600,300])
+
+# into.show()
+
+# into = Image.open("Resources/final-cover-forest.jpg")
+# print( into.size )
+# def draw_text(text: str, into, at: tuple):
+#
+#     f = ImageFont.truetype ('ttf/Merriweather-BoldItalic.ttf', 100)
+#     # image.new( mode, size(A x B))
+#     txt=Image.new('L', (500,500))
+#     d = ImageDraw.Draw(txt)
+#     d.text( at, text,  font=f, fill=100)
+#
+#     w=txt.rotate(55,  expand=1)
+#
+#     into.paste( ImageOps.colorize(w, (0,0,0), (0,0,0)), (242,60),  w)
+#     into.show()
+#
+# draw_text('hi wolf', into, (0,0))
+
+
+
+
+# Rotate Image By 180 Degree
+rotated_image1 = into.rotate(10)
+
+# This is Alternative Syntax To Rotate
+# The Image
+rotated_image2 = into.transpose(Image.ROTATE_90)
+
+# This Will Rotate Image By 60 Degree
+rotated_image3 = into.rotate(60)
+
+rotated_image1.show()
+# rotated_image2.show()
+# rotated_image3.show()
+rotated_image1.save('rolex.png')

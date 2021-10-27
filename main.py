68cb5fe86605c78114ccd90e70a47b5d1b9b1550
Wolf0605 <mmd0a605@naver.com>
2021-10-27 12:24:14 +0900

    plus ocr detecting, transalte

---
+++ main.py
@@ -0,0 +1,136 @@
+import cv2
+import matplotlib.pyplot as plt
+import easyocr
+import sys
+import googletrans
+from typing import List
+import requests
+import pprint
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print(cv2.__version__)
+
+# 이미지 파일 경로
+file_path = 'easy-ocr-project/patient.jpg'
+img = cv2.imread(file_path, cv2.IMREAD_COLOR)
+
+CLIENT_ID = "MawiiHEojSbWlRvZjWEM"
+CLIENT_SECRET = "gY1PNWHP54"
+
+if img is None:
+    print('Image load failed!')
+    sys.exit()
+
+
+# 이미지 출력함수
+def display(img):
+    # img_rgb = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
+    plt.figure(figsize=(15, 15))
+    plt.imshow(img)
+    plt.show()
+
+
+def cleanup_text(text):
+    # strip out non-ASCII text so we can draw the text on the image
+    # using OpenCV
+    return "".join([c if ord(c) < 128 else "" for c in text]).strip()
+
+
+def easy_ocr_result(img, language='en', draw=True, text=False):
+    reader = easyocr.Reader([language])
+    results = reader.readtext(img)
+
+    # 바운딩박스 리스트
+    bbox_list = []
+    # 텍스트 리스트
+    text_list = []
+
+    if draw == False: # 원래 이미지만 출력
+        display(img)
+
+    elif draw == True and text == False: # 이미지에 바운딩 박스그리기
+        img2 = img.copy()
+        # img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
+        for (bbox, text, prob) in results:
+            # display the OCR'd text and associated probability
+            # print("[INFO] {:.4f}: {}".format(prob, text))
+
+            bbox_list.append(bbox)
+            text_list.append(text)
+            # unpack the bounding box
+            (tl, tr, br, bl) = bbox
+            tl = (int(tl[0]), int(tl[1]))
+            tr = (int(tr[0]), int(tr[1]))
+            br = (int(br[0]), int(br[1]))
+            bl = (int(bl[0]), int(bl[1]))
+            # cleanup the text and draw the box surrounding the text along
+            # with the OCR'd text itself
+            cv2.rectangle(img2, tl, br, (255, 0, 0), 2)
+
+        # show the output image
+        display(img2)
+
+    elif draw == True and text == True:  # 이미지에 바운딩 + 인식한 글자
+        img2 = img.copy()
+        # img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
+
+        for (bbox, text, prob) in results:
+            # display the OCR'd text and associated probability
+            # print("[INFO] {:.4f}: {}".format(prob, text))
+
+            bbox_list.append(bbox)
+            text_list.append(text)
+
+            # unpack the bounding box
+            (tl, tr, br, bl) = bbox
+            tl = (int(tl[0]), int(tl[1]))
+            tr = (int(tr[0]), int(tr[1]))
+            br = (int(br[0]), int(br[1]))
+            bl = (int(bl[0]), int(bl[1]))
+            # cleanup the text and draw the box surrounding the text along
+            # with the OCR'd text itself
+            text = cleanup_text(text)
+            cv2.rectangle(img2, tl, br, (255, 0, 0), 2)
+            cv2.putText(img2, text, (tl[0], tl[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+
+        # show the output image
+        display(img2)
+    return bbox_list, text_list
+
+
+def translate_texts(texts: List[str], type='google') -> List[str]:
+    global tranlated_texts
+    if type == 'google':
+        translator = googletrans.Translator()
+        tranlated_texts = [
+            translator.translate(text=text, src='en', dest='ko').text
+            for text in texts
+        ]
+    elif type == 'naver':
+        url = "https://openapi.naver.com/v1/papago/n2mt"
+        header = {"X-Naver-Client-Id": CLIENT_ID, "X-Naver-Client-Secret": CLIENT_SECRET}
+        tranlated_texts = []
+        for text in texts:
+            data = {'text': text, 'source': 'en', 'target': 'ko'}
+            response = requests.post(url, headers=header, data=data)
+            rescode = response.status_code
+            if rescode == 200:
+                t_data = response.json()
+                tranlated_texts.append(t_data['message']['result']['translatedText'])
+            else:
+                print("Error Code:", rescode)
+
+    return tranlated_texts
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    _, text_list = easy_ocr_result(img)
+    print('Text_list :', text_list)
+    tranlated_texts: List[str] = translate_texts(texts=text_list, type='naver')
+    print(f'Tranlated_texts : {tranlated_texts}')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

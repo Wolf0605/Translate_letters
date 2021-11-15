9f20813bb0fb9e4cd7d1e7e3d73171f41990a5c2
Wolf0605 <82213429+Wolf0605@users.noreply.github.com>
2021-11-16 02:07:02 +0900

    ..

--- TEST_JW/testfile.py
+++ TEST_JW/testfile.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from sklearn.cluster import KMeans
 file_path = r'dead_endjpg.jpg'
 img = cv2.imread(file_path)
 
@@ -42,4 +43,55 @@ def call():
     cv2.imshow('img', img)
     cv2.waitKey(0)
 
-call()
+img = img.reshape((img.shape[0] * img.shape[1], 3)) # height, width 통합
+
+k = 5 # 예제는 5개로 나누겠습니다
+clt = KMeans(n_clusters = k)
+clt.fit(img)
+
+for center in clt.cluster_centers_:
+    print(center)
+
+def centroid_histogram(clt):
+    # grab the number of different clusters and create a histogram
+    # based on the number of pixels assigned to each cluster
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+
+    # normalize the histogram, such that it sums to one
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    # return the histogram
+    return hist
+
+
+hist = centroid_histogram(clt)
+
+print(hist)
+def plot_colors(hist, centroids):
+    # initialize the bar chart representing the relative frequency
+    # of each of the colors
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+
+    # loop over the percentage of each cluster and the color of
+    # each cluster
+    for (percent, color) in zip(hist, centroids):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+
+    # return the bar chart
+    return bar
+
+bar = plot_colors(hist, clt.cluster_centers_)
+
+
+# show our color bart
+plt.figure()
+plt.axis("off")
+plt.imshow(bar)
+plt.show()
